bed6a64543fccec8735bf7ae9726be4e41a20df9
MAHALAKSHMI150506 <mahalakshmikan1505@gmail.com>
2026-03-24 11:42:35 +0530

    Fix Express 5 wildcard syntax with regex

--- server/server.js
+++ server/server.js
@@ -55,9 +55,10 @@ initRiskScoreJob();
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('/*', (req, res) => {
+    app.get('(.*)', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
+
 
 } else {
     app.get('/', (req, res) => {
